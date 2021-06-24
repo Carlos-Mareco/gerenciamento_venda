99e0b67dad94c86bfdc6f3efd5acfb402fbd8a04
Carlos Mareco <carlosmareco2001@gmail.com>
2021-06-24 20:52:25 -0300

    Alteração teste

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Loja loja = new Loja("Loja genérica", "12345678000101");
 
-        loja.novoFuncionario("Carlos Henrique", "012345678910", "123", 2);
+        loja.novoFuncionario("Teste", "012345678910", "123", 2);
 
         Menu.principal(loja);
     }
